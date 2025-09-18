f95459d3c16e541b2daeb33de35acfc7156c9927
wistoria <aymar5039@gmail.com>
2025-09-18 14:07:46 +0100

    change api base url

--- src/config/global.ts
+++ src/config/global.ts
@@ -1,5 +1,5 @@
-export const API_BASE_URL = "http://127.0.0.1:8000/api/";
-// export const API_BASE_URL = "http://194.164.77.56/aurlus/api/";
+// export const API_BASE_URL = "http://127.0.0.1:8000/api/";
+export const API_BASE_URL = "http://194.164.77.56/aurlus/api/";
 export const TOKEN_STORAGE_KEY = "adminToken";
 export const API_ENDPOINT = {
     login : "auth/login",
