bfd152864520fdccfa99c75737cfaed99466c910
Wistoria <aymar5039@gmail.com>
2025-10-09 23:02:09 +0100

    OmaLAY

--- src/config/global.ts
+++ src/config/global.ts
@@ -1,5 +1,5 @@
-export const API_BASE_URL = "http://127.0.0.1:8000/api/";
-// export const API_BASE_URL = "https://aurlus-api.ai4smartbusiness.com/api/";
+// export const API_BASE_URL = "http://127.0.0.1:8000/api/";
+export const API_BASE_URL = "https://aurlus-api.ai4smartbusiness.com/api/";
 export const TOKEN_STORAGE_KEY = "adminToken";
 export const API_ENDPOINT = {
     login : "auth/login",
